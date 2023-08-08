fc70ec85ca76e8ac27268caa7bf95a626f047163
Yevgeniya Alexeyeva <y.alekseyeva@gmail.com>
2023-08-08 12:18:07 +0200

    clear config

--- configs/index.js
+++ configs/index.js
@@ -1,12 +1,6 @@
 const path = require("path");
 
 module.exports = {
-  logger: {
-    logPath: process.env.LOGGER_LOG_PATH,
-    printToConsole: process.env.LOGGER_PRINT_TO_CONSOLE === "true",
-    printToFile: process.env.LOGGER_PRINT_TO_FILE === "true",
-    consoleJson: process.env.LOGGER_CONSOLE_FORMAT_JSON === "true",
-  },
   server: {
     host: process.env.SERVER_HOST ?? "localhost",
     port: process.env.SERVER_PORT ?? "3000",
@@ -27,20 +21,5 @@ module.exports = {
     port: process.env.DATABASE_PORT ?? 3306,
     dialect: process.env.DATABASE_DIALECT ?? "mariadb",
     forceSync: process.env.DATABASE_FORCE_SYNC === "true",
-  },
-  instanceInfo: {
-    name: process.env.INSTANCE_INFO_NAME,
-    backendURL: process.env.INSTANCE_INFO_BACKEND_URL,
-    webURL: process.env.INSTANCE_INFO_WEB_URL,
-    webProtocol: process.env.INSTANCE_INFO_WEB_PROTOCOL,
-    webDomain: process.env.INSTANCE_INFO_WEB_DOMAIN,
-    webPort: process.env.INSTANCE_INFO_WEB_PORT,
-  },
-  icd: {
-    host: process.env.ICD_API_HOSTNAME,
-    authHost: process.env.ICD_AUTH_HOSTNAME,
-  },
-  media: {
-    rootFolder: process.env.MEDIA_ROOT_FOLDER,
   },
 };
